058e27bfe41ca9d72bbffbda701a17ed57835be4
xig1517 <xig1517@gmail.com>
2024-12-14 00:02:20 +0800

    Modify: 修改fileReader為fileManager, 並加入其他實用功能

--- data/fileManager.py
+++ data/fileManager.py
@@ -1,21 +1,26 @@
 import json
+import os
 
-class FileReader:
+class FileManager:
     
     def read(path: str):
         with open(path, 'r') as file:
             ret = file.read()
-        return ret
-
-    def readJson(path: str):
-        with open(path) as jfile:
-            ret = json.load(jfile)
         return ret
     
     def update(path: str, content: str):
         with open(path, "w") as file:
             file.write(content)
             
+    def readJson(path: str):
+        with open(path) as jfile:
+            ret = json.load(jfile)
+        return ret
+            
     def updateJson(path: str, content: str):
         with open(path, "w") as jfile:
             json.dump(content, jfile)
+
+
+dataPath = os.path.dirname(__file__)
+config = FileManager.readJson(os.path.dirname(__file__) + "/config.json")
