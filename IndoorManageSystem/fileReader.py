f152f1702fe1293f65076e220a3b41aacdecd0e0
xig1517 <xig1517@gmail.com>
2024-12-13 14:40:29 +0800

    Feat: 新增FileReader - 簡化讀寫檔案的過程

---
+++ IndoorManageSystem/fileReader.py
@@ -0,0 +1,17 @@
+import json
+
+class FileReader:
+    
+    def read(path: str):
+        with open(path, 'r') as f:
+            ret = f.read()
+        return ret
+
+    def readJson(path: str):
+        with open(path) as jfile:
+            ret = json.load(jfile)
+        return ret
+    
+    def update(path: str, content: str):
+        with open(path, "w") as f:
+            f.write(content)
