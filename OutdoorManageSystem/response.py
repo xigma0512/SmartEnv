a3800b000adfe9153f08b836667791206bb779b0
xig1517 <xig1517@gmail.com>
2024-12-16 01:44:36 +0800

    Chore: 刪除未使用的程式碼

--- OutdoorManageSystem/response.py
+++ OutdoorManageSystem/response.py
@@ -3,53 +3,10 @@ from OutdoorManageSystem.__types__ import Table, History
 
 from datetime import datetime, timedelta
 
-def isInRange(value: float, standard: float, deviation: float):
-    return abs(value - standard) <= deviation
-
-# return [0: 什麼都不做, 1: 開窗, 2: 開冷氣, 3: 開暖氣, 4: Error]
-def tempChecker():
-    return None
-    to_date = datetime.now().strftime("%Y-%m-%d")
-    
-    indoorHistory: History = FileManager.readJson(dataPath + "/indoor/history.json")
-    outdoorHistory: History = FileManager.readJson(dataPath + "/outdoor/history.json")
-    conf = config['indoorManageSystem']
-    
-    indoorAT, outdoorAT = indoorHistory[to_date][-1]['temp'], outdoorHistory[to_date][-1]['temp']
-    standardTemp, deviationTemp = conf['standard_temp'], conf['deviation_temp']
-    
-    if isInRange(indoorAT, standardTemp, deviationTemp): return 0
-    
-    if isInRange(outdoorAT, standardTemp, deviationTemp): return 1
-    if indoorAT > standardTemp:
-        if outdoorAT < standardTemp: return 1
-        return 2
-    else:        
-        if outdoorAT > standardTemp: return 1
-        return 3
-
-# return [0: 什麼都不做, 1: 開啟除濕機, 2: 開啟加濕器]
-def moistChecker():
-    return None
-    to_date = datetime.now().strftime("%Y-%m-%d")
-    
-    indoorHistory: History = FileManager.readJson(dataPath + "/indoor/history.json")
-    conf = config['indoorManageSystem']
-    
-    indoorAM = indoorHistory[to_date][-1]['moist']
-    standardMoist, deviationMoist = conf['standard_temp'], conf['deviation_temp']
-
-    if isInRange(indoorAM, standardMoist, deviationMoist): return 0
-    
-    if indoorAM > standardMoist: return 1
-    else: return 2
-
-
 def response():
     
     retMessage = {
         'realTimeData': [0,0,0],
-        #'control': [tempChecker(), moistChecker()],
         'history': {
             'labels': [], # hours
             'temp': [], # values
